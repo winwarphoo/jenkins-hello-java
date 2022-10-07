c9aaaef834f4247eda28aa9b0d048e525ff91930
winwarphoo <winwarphoo@mm.cybermissions.co.jp>
2022-10-07 17:39:50 +0900

    update BillCalculator

--- src/main/java/com/example/smartphone/BillCalculator.java
+++ src/main/java/com/example/smartphone/BillCalculator.java
@@ -1,20 +1,43 @@
 package com.example.smartphone;
 
+import java.util.Arrays;
+
+import com.example.smartphone.options.CompensationService;
+import com.example.smartphone.options.NetSecurity;
+import com.example.smartphone.options.RemoteSupport;
 
 /**
  * BillCalculator
 */
 public class BillCalculator {
 	
-	public static int getBillPerMonth(Option... option) {
-		int bill = 0;
-    if (option.length == 3) {
-      bill = 1000;
-    } else {
-      for (Option op : option) {
-        bill += op.getBillPerMonth();
-      }
-    }
-    return bill;
+	public static int getBillPerMonth(Option... options) {
+		if (isOptionPack(options)) {
+			return 1000;
+		} else {
+			return Arrays.asList(options).stream()
+				.map(opt -> opt.getBillPerMonth())
+				.reduce(0, (x, y) -> x + y);
+		}
+		// int bill = 0;
+    // if (option.length == 3) {
+    //   bill = 1000;
+    // } else {
+    //   for (Option op : option) {
+    //     bill += op.getBillPerMonth();
+    //   }
+    // }
+    // return bill;
+		}
+
+	private static boolean isOptionPack(Option[] options) {
+		return Arrays.asList(options).stream()
+			.anyMatch(opt -> opt instanceof CompensationService)
+			&&
+			Arrays.asList(options).stream()
+			.anyMatch(opt -> opt instanceof RemoteSupport)
+			&&
+			Arrays.asList(options).stream()
+			.anyMatch(opt -> opt instanceof NetSecurity);
   }
 }
